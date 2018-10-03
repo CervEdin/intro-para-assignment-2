b422d5bf34033aebb56d3183295a35a96e18aede
Erik Cervin-Edin <erik@cervined.in>
2018-10-03 04:14:58 +0200

    Add sequential implementation

--- 2-sieve.cpp
+++ 2-sieve.cpp
@@ -1,10 +1,10 @@
 // For this exercise, you will use Posix threads to implement a parallel version of the Sieve of Eratosthenes, an algorithm to find prime numbers. For some maximum positive integer M ax, the Sieve of Eratosthenes works as follows:
 
 /*
-1. Create a list of natural numbers: 1, 2, 3, ... , Max.
+
 2. Set k to 2, the first unmarked number in the list.
 3. 	repeat
-		Mark all multiples of k between k 2 and Max.
+		Mark all multiples of k between k^2 and Max.
 		Find the smallest number greater than k that is still unmarked.
 		Set k to this new value.
 	until k^2 is greater than Max.
@@ -26,6 +26,26 @@
 #include <iostream>
 #include <thread>
 
+void sequential_sieve(int natural_numbers[], int k, int max)
+{
+	// int k_index = k - 1;
+	int k_squared = k*k;
+	// repeat until k^2 is greater than max
+	while (k_squared < max) {
+		// Mark all multiples of k between k^2 and Max
+		for (int i = k_squared - 1; i < max; i += k)
+		{
+			natural_numbers[i] = 0;
+		}
+		// Find the smallest number greater than k that is still unmarked.
+		while (natural_numbers[k] == 0) {
+			k++;
+		}
+		k = natural_numbers[k];
+		k_squared = k*k;
+	}
+}
+
 void usage(char *program)
 {
 	std::cout << "Usage: " << program << " T M" << std::endl;
@@ -41,24 +61,49 @@ int main(int argc, char *argv[])
 	{
 		usage(argv[0]);
 	}
-	int upper_bound;
-	try
-	{
-		upper_bound = std::stoi(argv[1]);
-	}
-	catch (std::exception)
-	{
-		usage(argv[0]);
-	}
 	int threads;
 	try
 	{
-		threads = std::stoi(argv[2]);
+		threads = std::stoi(argv[1]);
 	}
 	catch (std::exception)
 	{
 		usage(argv[0]);
 	}
+	int upper_bound;
+	try
+	{
+		upper_bound = std::stoi(argv[2]);
+	}
+	catch (std::exception)
+	{
+		usage(argv[0]);
+	}
+
+
+	// 1. Create a list of natural numbers: 1, 2, 3, ... , Max.
+	int natural_numbers[upper_bound];
+	for (int i = 0, j = 1; i < upper_bound; i++, j++) 
+	{
+		natural_numbers[i] = j;
+	}
+	if (threads == 1) {
+		sequential_sieve(natural_numbers, 2, upper_bound);
+	}
+	else {
+		for (int i=0; i<threads; i++)
+		{
+
+		}
+	}
+
+	int nr_primes = 0;
+	for (int i = 1; i < upper_bound; i++)
+	{
+		nr_primes += natural_numbers[i] ? 1 : 0;
+	}
+
+    std::cout << "There were " << nr_primes << " primes." << std::endl;
 
 	return 0;
 }
